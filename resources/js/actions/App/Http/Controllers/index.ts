efc632f84c2acc920aef1266f914863b35349c9c
Heak Longdy <longdyheak.morakot@gmail.com>
2026-09-08 11:10:13 +0700

    fix: track wayfinder generated routes and actions for vercel build

---
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -0,0 +1,9 @@
+import CustomerController from './CustomerController'
+import Settings from './Settings'
+
+const Controllers = {
+    CustomerController: Object.assign(CustomerController, CustomerController),
+    Settings: Object.assign(Settings, Settings),
+}
+
+export default Controllers
